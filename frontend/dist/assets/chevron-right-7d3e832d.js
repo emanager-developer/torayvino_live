44f955f79809b78ab2e6790456be11e2fdaa92e3
MMamunurRashid <mdmamun.iubat.m@gmail.com>
2026-01-18 12:12:41 +0600

    frontend update for suspense issue

---
+++ frontend/dist/assets/chevron-right-7d3e832d.js
@@ -0,0 +1,6 @@
+import{s as o}from"./index-e0eec1b4.js";/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],c=o("chevron-right",t);export{c as C};
